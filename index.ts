414d5a87ae0b4afe638c5c2dc8ae853b10a8a4ae
Ayomide <ogunyemiayomide700@gmail.com>
2023-11-10 15:21:12 +0100

    process

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+console.clear();
 import express, { Application } from "express";
 
 const app: Application = express();
@@ -6,4 +7,16 @@ const port = 4242;
 
 const server = app.listen(port, () => {
   console.log("server is live on ", port);
+});
+
+process.on("uncaughtException", (error: Error) => {
+  console.log("uncaughtException", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason: any) => {
+  console.log("unhandledRejection", reason);
+  server.listen(() => {
+    process.exit(1);
+  });
 });
